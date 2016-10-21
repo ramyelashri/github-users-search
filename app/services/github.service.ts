7d719216e835467e5f8dd8028190918796ef7ffc
Ramy Elashri <ramyelashri@gmail.com>
2016-10-21 04:19:08 +0200

    Add search functionality and user profile markup

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -22,4 +22,18 @@ export class GithubService {
             + '&client_secret='
             + this.client_secret).map(res => res.json())
     }
+
+    getRepos() {
+        return this._http.get(
+            'http://api.github.com/users/'
+            + this.username
+            + '/repos?client_id='
+            + this.client_id
+            + '&client_secret='
+            + this.client_secret).map(res => res.json())
+    }
+
+    updateUser(username:string) {
+        this.username = username;
+    }
 }
